9e38ddb056f34cf7eb42eba4708a1bf1d6744ba4
James George <25279263+jamesgeorge007@users.noreply.github.com>
2025-12-31 14:22:56 +0530

    feat: add protocol switcher to request tabs
    Added a protocol switcher button bar at the top of both REST and GraphQL request tabs,
    allowing users to easily switch between protocols (REST <-> GraphQL) on the same tab.
    This completes the unified REST/GraphQL view by providing a visible way to toggle
    between protocols.

--- packages/hoppscotch-common/src/components/graphql/RequestTab.vue
+++ packages/hoppscotch-common/src/components/graphql/RequestTab.vue
@@ -1,18 +1,43 @@
 <template>
-  <AppPaneLayout layout-id="gql-primary">
-    <template #primary>
-      <GraphqlRequestOptions
-        v-model="tab.document.request"
-        v-model:response="tab.document.response"
-        v-model:option-tab="tab.document.optionTabPreference"
-        v-model:inherited-properties="tab.document.inheritedProperties"
-        :tab-id="tab.id"
-      />
-    </template>
-    <template #secondary>
-      <GraphqlResponse :response="tab.document.response" />
-    </template>
-  </AppPaneLayout>
+  <div class="flex flex-col h-full">
+    <!-- Protocol Switcher -->
+    <div class="flex items-center gap-2 px-4 py-2 border-b border-dividerLight bg-primary">
+      <span class="text-xs font-semibold text-secondaryLight">{{ t("label.protocol") }}:</span>
+      <div class="flex gap-1">
+        <button
+          class="px-3 py-1 text-xs font-medium rounded transition-colors bg-surface2 text-secondary hover:bg-surface1"
+          @click="switchProtocol('rest')"
+        >
+          REST
+        </button>
+        <button
+          class="px-3 py-1 text-xs font-medium rounded transition-colors"
+          :class="{
+            'bg-accent text-accentContrast': true,
+            'bg-surface2 text-secondary hover:bg-surface1': false,
+          }"
+          @click="switchProtocol('graphql')"
+        >
+          GraphQL
+        </button>
+      </div>
+    </div>
+
+    <AppPaneLayout layout-id="gql-primary">
+      <template #primary>
+        <GraphqlRequestOptions
+          v-model="tab.document.request"
+          v-model:response="tab.document.response"
+          v-model:option-tab="tab.document.optionTabPreference"
+          v-model:inherited-properties="tab.document.inheritedProperties"
+          :tab-id="tab.id"
+        />
+      </template>
+      <template #secondary>
+        <GraphqlResponse :response="tab.document.response" />
+      </template>
+    </AppPaneLayout>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,8 +47,15 @@ import { watch } from "vue"
 import { isEqualHoppGQLRequest } from "~/helpers/graphql"
 import { HoppGQLDocument } from "~/helpers/graphql/document"
 import { HoppTab } from "~/services/tab"
+import { useI18n } from "~/composables/i18n"
+import { createDefaultRESTDocument } from "~/helpers/unified/document"
+import { useService } from "dioc/vue"
+import { UnifiedTabService } from "~/services/tab/unified"
 
 // TODO: Move Response and Request execution code to over here
+
+const t = useI18n()
+const tabs = useService(UnifiedTabService)
 
 const props = defineProps<{ modelValue: HoppTab<HoppGQLDocument> }>()
 
@@ -32,6 +64,20 @@ const emit = defineEmits<{
 }>()
 
 const tab = useVModel(props, "modelValue", emit)
+
+// Protocol switcher
+const switchProtocol = (protocol: "rest" | "graphql") => {
+  if (protocol === "graphql") {
+    return // Already on GraphQL
+  }
+
+  // Switch to REST
+  const newTab = {
+    ...tab.value,
+    document: createDefaultRESTDocument(),
+  }
+  tabs.updateTab(newTab)
+}
 
 // TODO: Come up with a better dirty check
 let oldRequest = cloneDeep(tab.value.document.request)
--- packages/hoppscotch-common/src/components/http/RequestTab.vue
+++ packages/hoppscotch-common/src/components/http/RequestTab.vue
@@ -1,21 +1,46 @@
 <template>
-  <AppPaneLayout layout-id="rest-primary">
-    <template #primary>
-      <HttpRequest v-model="tab" />
-      <HttpRequestOptions
-        v-model="tab.document.request"
-        v-model:option-tab="tab.document.optionTabPreference!"
-        v-model:inherited-properties="tab.document.inheritedProperties"
-      />
-    </template>
-    <template #secondary>
-      <HttpResponse
-        v-model:document="tab.document"
-        :tab-id="tab.id"
-        :is-embed="false"
-      />
-    </template>
-  </AppPaneLayout>
+  <div class="flex flex-col h-full">
+    <!-- Protocol Switcher -->
+    <div class="flex items-center gap-2 px-4 py-2 border-b border-dividerLight bg-primary">
+      <span class="text-xs font-semibold text-secondaryLight">{{ t("label.protocol") }}:</span>
+      <div class="flex gap-1">
+        <button
+          class="px-3 py-1 text-xs font-medium rounded transition-colors"
+          :class="{
+            'bg-accent text-accentContrast': true,
+            'bg-surface2 text-secondary hover:bg-surface1': false,
+          }"
+          @click="switchProtocol('rest')"
+        >
+          REST
+        </button>
+        <button
+          class="px-3 py-1 text-xs font-medium rounded transition-colors bg-surface2 text-secondary hover:bg-surface1"
+          @click="switchProtocol('graphql')"
+        >
+          GraphQL
+        </button>
+      </div>
+    </div>
+
+    <AppPaneLayout layout-id="rest-primary">
+      <template #primary>
+        <HttpRequest v-model="tab" />
+        <HttpRequestOptions
+          v-model="tab.document.request"
+          v-model:option-tab="tab.document.optionTabPreference!"
+          v-model:inherited-properties="tab.document.inheritedProperties"
+        />
+      </template>
+      <template #secondary>
+        <HttpResponse
+          v-model:document="tab.document"
+          :tab-id="tab.id"
+          :is-embed="false"
+        />
+      </template>
+    </AppPaneLayout>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,8 +50,15 @@ import { cloneDeep } from "lodash-es"
 import { isEqualHoppRESTRequest } from "@hoppscotch/data"
 import { HoppTab } from "~/services/tab"
 import { HoppRequestDocument } from "~/helpers/rest/document"
+import { useI18n } from "~/composables/i18n"
+import { createDefaultGQLDocument } from "~/helpers/unified/document"
+import { useService } from "dioc/vue"
+import { UnifiedTabService } from "~/services/tab/unified"
 
 // TODO: Move Response and Request execution code to over here
+
+const t = useI18n()
+const tabs = useService(UnifiedTabService)
 
 const props = defineProps<{ modelValue: HoppTab<HoppRequestDocument> }>()
 
@@ -35,6 +67,20 @@ const emit = defineEmits<{
 }>()
 
 const tab = useVModel(props, "modelValue", emit)
+
+// Protocol switcher
+const switchProtocol = (protocol: "rest" | "graphql") => {
+  if (protocol === "rest") {
+    return // Already on REST
+  }
+
+  // Switch to GraphQL
+  const newTab = {
+    ...tab.value,
+    document: createDefaultGQLDocument(),
+  }
+  tabs.updateTab(newTab)
+}
 
 // TODO: Come up with a better dirty check
 let oldRequest = cloneDeep(tab.value.document.request)
